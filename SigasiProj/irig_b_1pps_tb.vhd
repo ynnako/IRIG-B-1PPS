74e0b19905199c023ee1e429c8d0282d2eceaaac
Yonatan <ynnako@gmail.com>
2019-11-19 14:16:45 +0200

    added an option for phase shift in both clocks

--- SigasiProj/irig_b_1pps_tb.vhd
+++ SigasiProj/irig_b_1pps_tb.vhd
@@ -12,13 +12,14 @@ architecture RTL of irig_b_1pps_tb is
 	signal s_irig_clk : std_logic := '1';
 	signal s_rst : std_logic := '1';
 	signal s_irig_data_in : std_logic;
-	signal s_valid :std_logic;
+	signal s_one_pps : std_logic;
 begin
 
 s_rst <= '0' after 110 ns;
 
 	rx_clk_gen_proc : process
 	begin
+		wait for c_phase_rx_clk;
 		wait for c_rx_clk_half_period;
 		s_1pps_clk <= not s_1pps_clk;
 
@@ -26,18 +27,26 @@ s_rst <= '0' after 110 ns;
 	
 	irig_clk_gen_proc : process
 	begin
+		wait for c_phase_irig_clk;
 		wait for c_irig_b_clk_half_period;
 		s_irig_clk <= not s_irig_clk;
 
 	end process irig_clk_gen_proc;
 	
+	u_one_pps : entity work.irig_sync
+		port map(
+			CLK     => s_1pps_clk,
+			RST     => s_rst,
+			DATA_IN => s_irig_data_in,
+			ONE_PPS => 
+			s_one_pps
+		);
 	
 	u_file_reader : entity work.msg_reader
 		port map(
 			CLK   => s_irig_clk,
 			RST   => s_rst,
-			DATA  => s_irig_data_in,
-			VALID => s_valid
+			DATA  => s_irig_data_in
 			);
 
 end architecture RTL;
--- SigasiProj/irig_b_1pps_tb_pack.vhd
+++ SigasiProj/irig_b_1pps_tb_pack.vhd
@@ -2,16 +2,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package irig_b_1pps_tb_pack is
-	
-	constant c_rx_clk_half_period : time := 2.5 ns;
+
+	constant c_rx_clk_half_period     : time := 5 ns;
 	constant c_irig_b_clk_half_period : time := 10 ns;
-	
+	constant c_phase_irig_clk         : time := 0 ns; --0.15 * c_irig_b_clk_half_period;
+	constant c_phase_rx_clk           : time := 0 ns; --0.37 * c_rx_clk_half_period;
+
 	constant c_init : std_logic := '1';
 	constant c_norm : std_logic := '0';
-	 
-	
+
 end package irig_b_1pps_tb_pack;
 
 package body irig_b_1pps_tb_pack is
-	
+
 end package body irig_b_1pps_tb_pack;
