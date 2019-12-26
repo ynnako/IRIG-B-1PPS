55b10ed6839774afcbbc8a8c5b06ad617051c9c0
ynnako <ynnako.junk@gmail.com>
2019-12-26 13:11:47 +0200

    created pps_gen

--- vhdl/pack/irig_b_pack.vhd
+++ vhdl/pack/irig_b_pack.vhd
@@ -4,13 +4,8 @@ use ieee.std_logic_1164.all;
 package irig_b_pack is
 	constant c_init : std_logic := '1';
 	constant c_norm : std_logic := '0';
-
-	constant c_first_rise  : std_logic := '0';
-	constant c_second_rise : std_logic := '1';
-
-	constant c_low_width  : integer := 4;
-	constant c_high_width : integer := 10;
-	constant c_ref_width  : integer := 16;
+	
+	constant c_frame_size : integer := 10;
 end package irig_b_pack;
 
 package body irig_b_pack is
